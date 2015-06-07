b322506a2040d14ce69c7932ff49edee45be3dd7
hooting <hooting@114-212-81-102.nju.edu.cn>
2015-06-07 14:31:17 +0800

    #12

---
+++ cpp_src/IntegerToRoman.cpp
@@ -0,0 +1,13 @@
+/**
+ * reference to https://leetcode.com/discuss/32626/simple-solution
+ */
+class Solution {
+public:
+    string intToRoman(int num) {
+        string M[] = {"", "M", "MM", "MMM"};
+        string C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        string X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        string I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    }
+};
