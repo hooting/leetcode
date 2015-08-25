e409dc16819462a1b8a4ff5fe00241c114b95322
hooting <hooting@Hootings-MacBook-Pro.local>
2015-08-25 22:44:42 +0800

    #258

---
+++ python_src/AddDigits.py
@@ -0,0 +1,9 @@
+#reference to https://en.wikipedia.org/wiki/Digital_root
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0: return 0
+        return 1 + (num - 1) % 9
